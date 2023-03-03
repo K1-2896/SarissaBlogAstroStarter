d19f64ccad8ed07fc42457b84450413cf7a4fad6
k1git <k1smz2896@gmail.com>
2023-03-03 13:11:17 +0900

    レフトカラム更新

--- src/sarissa.config.cjs
+++ src/sarissa.config.cjs
@@ -1,48 +1,46 @@
 export default {
-  siteTitle: "Sarissa Blog",
-  siteDescription: "Responsive Blog Template",
-  favicon: "/favicon.ico",
-  siteImagePath: "/images/sarissa.png",
-  footer: "© 1967 - 2022 Sarissa Blog",
-  dateFormat: "dd.MM.yyyy HH:mm",
+  siteTitle: 'Klog',
+  siteDescription: 'KEGのBLOG、略してKlogです。',
+  favicon: '/favicon.ico',
+  siteImagePath: '/images/MyBlogTitle.png',
+  footer: '© 2023 Klog',
+  dateFormat: 'dd.MM.yyyy HH:mm',
   socialMedia: {
-    facebook: "ozcelikismail",
-    twitter: "ismailozcelik",
-    linkedin: "ismail-özçelik",
-    github: "iozcelik",
+    twitter: 'KEG_0717',
+    github: 'K1-2896',
   },
   pageSize: 5,
   categories: [
     {
-      name: "theme",
-      color: "btn-warning",
-      image: "/images/theme.jpg",
+      name: 'theme',
+      color: 'btn-warning',
+      image: '/images/theme.jpg',
       order: 1,
     },
   ],
   categorySettings: {
-    order: "name", // name | count
-    layout: "card", //button | card
-    image: "",
-    color: "btn-primary",
+    order: 'name', // name | count
+    layout: 'card', //button | card
+    image: '',
+    color: 'btn-primary',
     countVisibility: true,
   },
   searchOptions: {
     includeScore: true,
     includeMatches: true,
     keys: [
-      { name: "title", weight: 3 },
-      { name: "description", weight: 2 },
+      { name: 'title', weight: 3 },
+      { name: 'description', weight: 2 },
     ],
   },
   i18n: {
     search: {
-      placeholder: "Search post title and description...",
+      placeholder: 'Search post title and description...',
     },
     archive: {
-      select: "Select Year",
+      select: 'Select Year',
     },
-    page: "Page",
-    resultFound: " result(s) found",
+    page: 'Page',
+    resultFound: ' result(s) found',
   },
 };
